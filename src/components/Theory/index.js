8bee44df74bbea905c88e9e163aab68f7663cbfd
airsorna <83644142+airsorna@users.noreply.github.com>
2023-06-13 15:10:16 +0200

    third commit

--- src/components/Theory/index.js
+++ src/components/Theory/index.js
@@ -16,7 +16,7 @@ const Theory = () => {
             <TheoryH2>Blog_______Posts.</TheoryH2>
               <TheoryH2>Week 3</TheoryH2>
               <TheoryP>Last year, I really struggled with understanding JavaScript and APIs, partly because they weren’t explained properly on how to use them and when to use them, so I skipped the whole section on JavaScript and APIs. I have taken the initiative to try and learn as much as I need to know this year. As part of that, I’ve watched the revision videos, which I somewhat understand, but I believe doing it practically will propel me to understanding these concepts much quicker.</TheoryP>
-              <TheoryP>For this week, I looked for inspiration for my internet art. Here's what I’ve found: href="https://www.arebyte.com/?gclid=CjwKCAjwq-WgBhBMEiwAzKSH6FcTnsY8Gy8KbePGDC3A4QoRGzAIKvQtdGYetJFORkYbUpNksoYGDBoCJ50QAvD_BwELinks to an external site.Links to an external site." target="_blank" https://www.arebyte.com/?gclid=CjwKCAjwq-WgBhBMEiwAzKSH6FcTnsY8Gy8KbePGDC3A4QoRGzAIKvQtdGYetJFORkYbUpNksoYGDBoCJ50QAvD_BwELinks to an external site.Links to an external site. and href=" https://aoi.com/Links to an external site.Links to an external site." target="_blank" https://aoi.com/Links to an external site.Links to an external site.</TheoryP>
+              <TheoryP>For this week, I looked for inspiration for my internet art. Here's what I’ve found:</TheoryP>
               <TheoryP>The first website is arebyte; this looks great visually, as you can see the 3D model creatures moving around. I think this is really nice and well designed as a concept for a website. The second website is AOI, which also looks visually pleasing because of its moving art and overall color theme. Both are inspiration and concepts I'd love to deploy on my own website. My first impression of internet art was that I thought it was the same as data art, but then I came to the realization that internet art is art distributed through the internet, unlike data art, where you manipulate data into art. So far, I like what I see from the example made in class.</TheoryP>
               <TheoryP>The reading "The Work of Art in the Age of the Internet" by David Carrier makes reference to Walter Benjamin’s The Work of Art in the Age of Mechanical Reproduction and challenges the concept of art, especially authenticity, because, according to Benjamin, the mechanical reproduction of a work of art is essentially defined by multiple copies, so it shatters the aura by interrupting these founding traditions. However, Carrier pushes back on this by saying, "The original physical object is now merely the source, just as the author’s manuscript is the source of a novel, and the reproduction has become a genuine replacement for that object. And so owning the original is like owning the manuscript of a novel—a thing of value for specialists but not for the general public." (Carrier, 2020:4-5). Both are valid points, and I agree with both perspectives.</TheoryP>
               <TheoryP>Reference List</TheoryP>
@@ -25,7 +25,7 @@ const Theory = () => {
               <TheoryH2>Week 4</TheoryH2>
               <TheoryP>Last year, I really struggled with understanding JavaScript and APIs, partly because they weren’t explained properly on how to use them and when to use them, so I skipped the whole section on JavaScript and APIs. I have taken the initiative to try and learn as much as I need to know this year. As part of that, I’ve watched the revision videos, which I somewhat understand, but I believe doing it practically will propel me to understanding these concepts much quicker.</TheoryP>
               <TheoryP>Convention and Context by Steven Mailloux discusses how conventions are dependent on context. He makes a valid point when he says, "We can discuss conventions as ways of acting or as accomplishments of meaning, not only in literary and critical texts but also in discourses circulating in other cultural and social spheres." (Mailloux,1983:409). What I understand this to mean is that conventions are not a set of rules that must be followed at all times, but rather that conventions work hand in hand with context. In any given context, there are a set of conventions to be followed. How do I relate this to my understanding of UI and UX? Similar to conventions, UI and UX have a set of conventions one has to follow when designing a website, but again, not everything will apply as context is important. Maybe the website you plan to create doesn’t have an "about" page, for example. This is why context is important.</TheoryP>
-              <TheoryP>For this week, I looked for inspiration for my internet art. Here's what I’ve found: href="https://www.arebyte.com/?gclid=CjwKCAjwq-WgBhBMEiwAzKSH6FcTnsY8Gy8KbePGDC3A4QoRGzAIKvQtdGYetJFORkYbUpNksoYGDBoCJ50QAvD_BwELinks to an external site." target="_blank" https://www.arebyte.com/?gclid=CjwKCAjwq-WgBhBMEiwAzKSH6FcTnsY8Gy8KbePGDC3A4QoRGzAIKvQtdGYetJFORkYbUpNksoYGDBoCJ50QAvD_BwELinks to an external site. and href="https://aoi.com/Links to an external site." target="_blank" https://aoi.com/Links to an external site.</TheoryP>
+              <TheoryP>For this week, I looked for inspiration for my internet art. Here's what I’ve found:</TheoryP>
               <TheoryP>The first website is arebyte; this looks great visually, as you can see the 3D model creatures moving around. I think this is really nice and well designed as a concept for a website. The second website is AOI, which also looks visually pleasing because of its moving art and overall color theme. Both are inspiration and concepts I'd love to deploy on my own website.</TheoryP>
               <TheoryP>Reference</TheoryP>
               <TheoryP>Mailloux, S., 1983. Convention and context. New Literary History, 14(2), pp.399-407.</TheoryP>
@@ -59,8 +59,8 @@ speaking, we think of art as a physical manifestation of a production of some so
 Before music streaming was a thing, people used to purchase records, and before 
 TV streaming, people would go watch plays at a theater. Although this is complex, it 
 remains noteworthy that art, like many other things traditionally, is of physical 
-production, whether that’s a painting, a picture, or music. We can acknowledge that 
-fact. Fast-forwarding to the present day, we don’t define art by its physical presence 
+production, whether thats a painting, a picture, or music. We can acknowledge that 
+fact. Fast-forwarding to the present day, we dont define art by its physical presence 
 in space and time, as Walter Benjamin wrote in his essay The Work of Art in the Age 
 of Mechanical Reproduction. "Our fine arts were developed, and their types and uses 
 were established, in times very different from the present, by men whose power of 
@@ -85,12 +85,12 @@ could make the argument that the value of art is now determined by an everexpan
 of meaning and context, the museum. Art is increasingly designed with 
 reproducibility in mind.
               </TheoryP>
-              <TheoryP>The website I find to interesting, and I wish to mirror to some effect is href='https://snr4.com.mx/' https://snr4.com.mx/  by SNR4 a creative developer from Mexico. His/her website titled SNR4 Portfolio is amazing and looks incredibly.</TheoryP>
+              <TheoryP>The website I find to interesting, and I wish to mirror to some effect is by SNR4 a creative developer from Mexico. His/her website titled SNR4 Portfolio is amazing and looks incredibly.</TheoryP>
               <TheoryH2>Conceptual</TheoryH2>
               <TheoryP>Beginning the analysis from a conceptual level, the website was intended to
-showcase other websites and work he’s involved with. A title, description, and link 
+showcase other websites and work hes involved with. A title, description, and link 
 are given for each website, and users can click on the link to visit the website. This is 
-unlike anything I’ve seen. Usually, with portfolio pages, web designers tend to write 
+unlike anything Ive seen. Usually, with portfolio pages, web designers tend to write 
 long text accompanied by images describing the work they have done along with 
 their accomplishments and experiences. This approach breaks that mold by having 
 an active showcase, as if one is in a museum moving from painting to painting.</TheoryP>
@@ -103,7 +103,7 @@ water, with 3D model astronauts viewing the image as if it were in a museum. The
 setup is like an exhibition, and the viewer moves from one art piece to another. I 
 think this is an amazing concept that is far from conventional because "net art 
 challenges preconceived notions of what the web is, how it works, and how users 
-participate and experience life online." (Park, 2007:4). SNR4’s concept of having the 
+participate and experience life online." (Park, 2007:4). SNR4s concept of having the 
 user interact with his portfolio by scrolling and clicking is a brilliant design element 
 that encourages the user to want to learn more about him and potentially inquire 
 about his services.
@@ -169,7 +169,7 @@ animation, camera movement, and video clips. When all these things come together
 they form this amazing and well-designed piece of net art.
 </TheoryP>
 <TheoryH2>Reference List</TheoryH2>
-<TheoryP>Artsy. (2023). Net Art. [online]. Available at: href='https://www.artsy.net/gene/net-art' https://www.artsy.net/gene/net-art [Accessed 28 April 2023]</TheoryP>
+<TheoryP>Artsy. (2023). Net Art. [online]. Available at:  [Accessed 28 April 2023]</TheoryP>
 <TheoryP>Benjamin, W. (1936). Hannah Arendt (ed.). The Work of Art in the Age of Mechanical 
 Reproduction. Illuminations. London: Fontana
 </TheoryP>
@@ -179,7 +179,7 @@ Grande do Norte Federal University.</TheoryP>
 <TheoryP>Park, Y.J. (2007). Design principles for interactive user experience through net art. 
 Australia. Central Queensland University.
 </TheoryP>
-<TheoryP>SNR4. (2023). Snr4 Portfolio. [online]. Available at: href='https://snr4.com.mx/'https://snr4.com.mx/ [Accessed 
+<TheoryP>SNR4. (2023). Snr4 Portfolio. [online]. Available at:  [Accessed 
 28 April 2023]</TheoryP>
 <TheoryP>Thorlacius, L. (2007). The Role of Aesthetics in Web Design. Denmark. Roskilde 
 University. pp. 63-76</TheoryP>
